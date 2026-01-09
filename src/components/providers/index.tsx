e05d6cf3961f7d376b847172c079e999555c9fed
Eder Rosa <eder_junior07@hotmail.com>
2026-01-09 14:58:10 -0300

    up

--- src/components/providers/index.tsx
+++ src/components/providers/index.tsx
@@ -13,8 +13,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
-      enableSystem
+      defaultTheme="light"
+      forcedTheme="light"
+      enableSystem={false}
       disableTransitionOnChange
     >
       <ORPCProvider>
