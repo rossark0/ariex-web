a1bc513c7317d468fa7d8623d12e6c1fa5ab0d37
Eder Rosa <eder_junior07@hotmail.com>
2026-01-09 14:55:17 -0300

    up

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -2,12 +2,11 @@
 
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
 
 /**
  * Theme provider component
  * Wraps the app to provide theme context
  */
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: any) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
